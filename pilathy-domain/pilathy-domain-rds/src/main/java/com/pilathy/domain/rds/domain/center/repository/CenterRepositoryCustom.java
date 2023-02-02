7302a1d199f5dcea3e0d1e462fb3b16ecc625f1e
wooyongjeong <wooyong.dev@gmail.com>
2023-02-03 01:46:31 +0900

    feat: [T-0006] querydsl 사용

---
+++ pilathy-domain/pilathy-domain-rds/src/main/java/com/pilathy/domain/rds/domain/center/repository/CenterRepositoryCustom.java
@@ -0,0 +1,9 @@
+package com.pilathy.domain.rds.domain.center.repository;
+
+import com.pilathy.domain.rds.domain.center.Center;
+
+public interface CenterRepositoryCustom {
+
+    Center findCenterById(Long centerId);
+
+}
