b061ec1fc43d36d20b948b477a16fbc13abb953f
wooyongjeong <wooyong.dev@gmail.com>
2023-02-03 01:46:31 +0900

    feat: [T-0006] querydsl 사용

--- pilathy-domain/pilathy-domain-rds/src/main/java/com/pilathy/domain/rds/domain/account/user/UserRepository.java
+++ pilathy-domain/pilathy-domain-rds/src/main/java/com/pilathy/domain/rds/domain/account/user/UserRepository.java
@@ -1,6 +1,7 @@
 package com.pilathy.domain.rds.domain.account.user;
 
+import com.pilathy.domain.rds.domain.account.user.repository.UserRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, UserRepositoryCustom {
 }
