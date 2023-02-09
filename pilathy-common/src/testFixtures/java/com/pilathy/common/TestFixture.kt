7273f0749d24972b5f1ca96d76536fdaf107da0a
wooyongjeong <wooyong.dev@gmail.com>
2023-02-10 01:04:32 +0900

    feat: [T-0010] 도메인 Test Fixture 추가

---
+++ pilathy-common/src/testFixtures/java/com/pilathy/common/TestFixture.kt
@@ -0,0 +1,8 @@
+package com.pilathy.common
+
+/**
+ * Identity Annotation
+ * - TextFixture (Object Mother)
+ * - An object mother is a kind of class used in testing to help create example objects that you use for testing.
+ */
+annotation class TestFixture
