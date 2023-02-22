5d062284534560abc0cba7e7d5620db07f537335
jiyeonhong <hjy5421@naver.com>
2023-02-22 23:45:06 +0900

    feat: [T-0014] reqeustDto 생상자 추가, responseDto 추가

--- pilathy-api/pilathy-api-user/src/main/java/com/pilathy/api/user/dto/user/UserResponse.java
+++ pilathy-api/pilathy-api-user/src/main/java/com/pilathy/api/user/dto/user/UserResponse.java
@@ -1,11 +1,9 @@
 package com.pilathy.api.user.dto.user;
 
-import com.pilathy.api.user.config.validator.Password;
 import com.pilathy.api.user.config.validator.Phone;
 import com.pilathy.domain.rds.domain.account.user.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.validation.constraints.Email;
@@ -13,17 +11,12 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
-
 @ToString
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
-public class UserRequest {
+public class UserResponse {
     @Email
     private String email;
-
-    @Password
-    private String password;
 
     @NotBlank
     @Size(max = 20)
@@ -34,7 +27,7 @@ public class UserRequest {
     @Phone
     private String phone;
 
-    public User toEntity() {
-        return User.of(email, password, name, birthDate, phone);
+    public static UserResponse of(User user) {
+        return new UserResponse(user.getEmail(), user.getName(), user.getBirthDate(), user.getPhone());
     }
 }
